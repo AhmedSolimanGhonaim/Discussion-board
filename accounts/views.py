0bb4995e629ee79cfd1fe2d8809cb6016927b647
ahmedghonaim <ahmedghonaim>
2025-04-09 21:36:40 +0200

    change password views and temp

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render , redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth import login, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import SignUpForm
 
 def signup(request):
@@ -18,8 +19,19 @@ def signup(request):
 @login_required
 def my_profile(request):
     return render(request, 'accounts/my_profile.html')
+
 @login_required
 def change_password(request):
+    # if request.method == 'POST':
+    #     form = PasswordChangeForm(user=request.user, data=request.POST)
+    #     if form.is_valid():
+    #         user = form.save()
+    #         update_session_auth_hash(request, user)
+    #         messages.success(request, 'Your password was successfully updated!')
+    #         return redirect('my_profile')
+    # else:
+    #     form = PasswordChangeForm(user=request.user)
+    return render(request, 'accounts/change_password.html', {})
 
 
-    
+
