4ff870b5ae1d7120139215a8ee32d4332b8913d0
ahmedghonaim <ahmedghonaim>
2025-04-10 18:51:06 +0200

    boards class based views

--- boards/urls.py
+++ boards/urls.py
@@ -1,13 +1,13 @@
+# /discussion_board/boards/urls.py
+
 from django.urls import path
-
-from . import views 
-
+from . import views
 
 urlpatterns = [
-    path('',views.home, name='home'),
-    path('about/', views.about, name= 'about'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
     path('boards/<int:id>', views.board_topics, name='board_topics'),
-    path('boards/<int:id>/new', views.new_topic, name='new_topic'),
-    path('boards/<int:board_id>/topics/<int:topic_id>', views.topic_page, name='topic_page'),
-    path('boards/<int:board_id>/topics/<int:topic_id>/reply', views.reply_topic, name='reply_topic'),
+    path('boards/<int:id>/new', views.NewTopicView.as_view(), name='new_topic'),
+    path('boards/<int:board_id>/topics/<int:topic_id>', views.TopicPageView.as_view(), name='topic_page'),
+    path('boards/<int:board_id>/topics/<int:topic_id>/reply', views.ReplyTopicView.as_view(), name='reply_topic'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse , Http404
 from .models import Board, Topic, Post
 from django.contrib.auth.models import User
 from .forms import NewTopicForm , ReplyForm
-from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.decorators import login_required 
+from django.utils.decorators import method_decorator
+from django.views.generic import View
 
 def home(request):
     boards = Board.objects.all()
@@ -15,18 +16,41 @@ def about(request):
     return render(request, 'about.html')
 
 def board_topics(request,id):
-    
-    #  try:
-    #      board = Board.objects.get(pk = id)
-    #  except Board.DoesNotExist:
-    #         raise Http404
     board = get_object_or_404(Board, pk = id)
     return render(request, 'topics.html', {'board': board})
 
-@login_required
-def new_topic(request, id): 
-    board = get_object_or_404(Board, pk = id)
-    if request.method == 'POST':
+# @login_required
+# def new_topic(request, id): 
+#     board = get_object_or_404(Board, pk = id)
+#     if request.method == 'POST':
+#         form = NewTopicForm(request.POST)
+#         if form.is_valid():  
+#             topic = form.save(commit=False)
+#             topic.board = board
+#             topic.created_by = request.user
+#             topic.save()
+
+#             Post.objects.create(
+#                 message=form.cleaned_data.get('message'),
+#                 topic=topic,
+#                 created_by=request.user
+#             )
+
+#             return redirect('board_topics', id=board.pk)
+#     else:
+#         form = NewTopicForm()
+
+#     return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+
+@method_decorator(login_required, name='dispatch')
+class NewTopicView(View):
+    def get(self,request,id):
+        board= get_object_or_404(Board, pk = id)
+        form = NewTopicForm()
+        return render(request, 'new_topic.html', {'board': board, 'form': form})
+    def post(self,request,id):
+        board= get_object_or_404(Board, pk = id)
         form = NewTopicForm(request.POST)
         if form.is_valid():  
             topic = form.save(commit=False)
@@ -41,21 +65,51 @@ def new_topic(request, id):
             )
 
             return redirect('board_topics', id=board.pk)
-    else:
-        form = NewTopicForm()
+        return render(request, 'new_topic.html', {'board': board, 'form': form})
 
-    return render(request, 'new_topic.html', {'board': board, 'form': form})
 
-def topic_page(request,board_id,topic_id):
-     topic = get_object_or_404(Topic,board__pk=board_id ,pk=topic_id)
-     topic.views += 1
-     topic.save()
-     return render(request, 'topic_page.html', {'topic': topic})
+# def topic_page(request,board_id,topic_id):
+#      topic = get_object_or_404(Topic,board__pk=board_id ,pk=topic_id)
+#      topic.views += 1
+#      topic.save()
+#      return render(request, 'topic_page.html', {'topic': topic})
 
-@login_required
-def reply_topic(request,board_id,topic_id):
-    topic = get_object_or_404(Topic,board__pk=board_id ,pk=topic_id)
-    if request.method == 'POST':
+
+
+class TopicPageView(View):
+    def get(self, request, board_id, topic_id):
+        topic = get_object_or_404(Topic, board__pk=board_id, pk=topic_id)
+        topic.views += 1
+        topic.save()
+        return render(request, 'topic_page.html', {'topic': topic})
+ 
+
+# @login_required
+# def reply_topic(request,board_id,topic_id):
+#     topic = get_object_or_404(Topic,board__pk=board_id ,pk=topic_id)
+#     if request.method == 'POST':
+#         form = ReplyForm(request.POST)
+#         if form.is_valid():
+#             reply = form.save(commit=False)
+#             reply.topic = topic
+#             reply.created_by = request.user
+#             reply.save()
+#             return redirect('topic_page', board_id=board_id, topic_id=topic_id)
+#     else:
+#         form = ReplyForm()
+#     return render(request, 'reply_topic.html', {'topic_reply': topic, 'form_reply': form})
+
+
+
+@method_decorator(login_required, name='dispatch')
+class ReplyTopicView(View):
+    def get(self, request, board_id, topic_id):
+        topic = get_object_or_404(Topic, board__pk=board_id, pk=topic_id)
+        form = ReplyForm()
+        return self.render_reply_form(request, topic, form)
+
+    def post(self, request, board_id, topic_id):
+        topic = get_object_or_404(Topic, board__pk=board_id, pk=topic_id)
         form = ReplyForm(request.POST)
         if form.is_valid():
             reply = form.save(commit=False)
@@ -63,6 +117,10 @@ def reply_topic(request,board_id,topic_id):
             reply.created_by = request.user
             reply.save()
             return redirect('topic_page', board_id=board_id, topic_id=topic_id)
-    else:
-        form = ReplyForm()
-    return render(request, 'reply_topic.html', {'topic_reply': topic, 'form_reply': form})
+        return self.render_reply_form(request, topic, form)
+
+    def render_reply_form(self, request, topic, form):
+        return render(request, 'reply_topic.html', {
+            'topic_reply': topic,
+            'form_reply': form
+        })
